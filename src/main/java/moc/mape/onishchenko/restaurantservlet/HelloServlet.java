282cd309f1929fd34e2a3d9534844790fa874af8
JIeIIIa <stealth@bigmir.net>
2019-11-13 17:57:14 +0200

    Add HelloWorld servlet

---
+++ src/main/java/moc/mape/onishchenko/restaurantservlet/HelloServlet.java
@@ -0,0 +1,16 @@
+package moc.mape.onishchenko.restaurantservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
